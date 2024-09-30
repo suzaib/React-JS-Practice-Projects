ee60427c790b30d092ea3443360a1909b8d00411
Suzaib <160600583+suzaib@users.noreply.github.com>
2024-09-30 10:34:17 +0530

    Add files via upload

---
+++ Brand Page/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import HeroSection from './components/hero';
+import Navigation from './components/Navigation';
+const App=()=>{
+    return (
+    <div>
+        <Navigation></Navigation>
+        <HeroSection></HeroSection>
+    </div>
+    );
+};
+
+export default App;
---
+++ Brand Page/src/components/hero.jsx
@@ -0,0 +1,28 @@
+const HeroSection=()=>{
+    return <main className="hero container">
+        <div className="hero-content">
+            <h1>YOUR FEET DESERVES THE BEST</h1>
+            <p>
+                YOUR FEET DESERVE THE BEST AND WE'RE HERE TO HELP YOU WITH OUR SHOES. YOUR FEET DESERVE THE BEST AND WE'RE HERE TO HELP YOU WITH OUR SHOES.
+            </p>
+        
+            <div className="hero-btn">
+                <button>Shop Now</button>
+                <button className="secondary-btn">Category</button>
+            </div>
+
+            <div className="shopping">
+                <p>Also Available On</p>
+                <div className="brand-icons">
+                    <img src="../public/images/flipkart.png" alt="amazon" />
+                    <img src="../public/images/amazon.png" alt="flipkart" />
+                </div>
+            </div>
+        </div>
+        <div className="hero-image">
+            <img src="../public/images/shoe_image.png" alt="shoeImg" />
+        </div>
+    </main>
+}
+
+export default HeroSection;
