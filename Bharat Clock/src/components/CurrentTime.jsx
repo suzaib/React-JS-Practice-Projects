6303cf077788d134e4c516d8a7ed00121c670065
Suzaib <160600583+suzaib@users.noreply.github.com>
2024-09-30 10:24:36 +0530

    Add files via upload

---
+++ Bharat Clock/src/components/CurrentTime.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { useState,useEffect } from 'react';
+
+const CurrentTime = () => {
+
+    const [time,setTime]=useState(new Date());
+
+    useEffect(()=>{
+        console.log("Interval has been setup");
+        const intervalId=setInterval(()=>{
+            setTime(new Date());
+        },1000)
+
+        return ()=>{
+            clearInterval(intervalId);
+            console.log("cancelled the interval");
+        }
+    },[])
+ 
+  return (
+    <p className='lead'>
+        This is the current time:{time.toLocaleDateString()}-{" "}
+        {time.toLocaleTimeString()}
+    </p>
+  )
+}
+
+export default CurrentTime
