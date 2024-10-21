39a3930d6bb0299e31703a1a6b52b6c3e52d7860
Suzaib <160600583+suzaib@users.noreply.github.com>
2024-10-22 01:54:15 +0530

    Update CurrentTime.jsx

--- Bharat Clock/src/components/CurrentTime.jsx
+++ Bharat Clock/src/components/CurrentTime.jsx
@@ -3,17 +3,19 @@ import { useState,useEffect } from 'react';
 
 const CurrentTime = () => {
 
+    //Utilising the useState Hook to store time and update it
     const [time,setTime]=useState(new Date());
 
+    //The useEffect has empty array passed to it , therefore it only runs when the component loads for the first time
     useEffect(()=>{
-        console.log("Interval has been setup");
+
+        //And the interval is made that runs every seconds updating the time state
         const intervalId=setInterval(()=>{
             setTime(new Date());
         },1000)
 
         return ()=>{
             clearInterval(intervalId);
-            console.log("cancelled the interval");
         }
     },[])
  
