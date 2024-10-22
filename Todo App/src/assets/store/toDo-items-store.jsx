a508a613eb3ace581a3527fa08c6a9faa97dfa88
Suzaib <160600583+suzaib@users.noreply.github.com>
2024-10-23 02:44:41 +0530

    Update toDo-items-store.jsx

--- Todo App/src/assets/store/toDo-items-store.jsx
+++ Todo App/src/assets/store/toDo-items-store.jsx
@@ -1,21 +1,26 @@
 import { createContext } from "react"
 import { useReducer } from "react";
 
-
+//Creating context store
 export const ToDoItemsContext= createContext({
     toDoItems:[],
     addNewItem:()=>{},
     deleteItem:()=>{},
 });
 
+//Creating the reducer function
 const toDoItemsReducer=(currToDoItems,action)=>{
     let newToDoItems=currToDoItems;
+
+    //Handling the NEW_ITEM action
     if(action.type==="NEW_ITEM"){
       newToDoItems=[
         ...currToDoItems,
         {name:action.payload.itemName,dueDate:action.payload.itemDueDate}
       ]
     }
+
+    //Handling the DELETE_ITEM action
     else if(action.type==="DELETE_ITEM"){
       newToDoItems=currToDoItems.filter((item)=>item.name!==action.payload.itemName);
   
@@ -24,6 +29,7 @@ const toDoItemsReducer=(currToDoItems,action)=>{
     return newToDoItems;
   }
 
+//Creating and exporting the provider
 export const ToDoItemsContextProvider=({children})=>{
     const [toDoItems,dispatchToDoItems]=useReducer(toDoItemsReducer,[]);
 
