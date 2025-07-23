3cf606be644b89ae65d36c076992aaae98041d74
Suzaib <160600583+suzaib@users.noreply.github.com>
2025-07-24 01:56:20 +0530

    Delete Amazon Zoom directory

--- Amazon Zoom/src/ZoomBox.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import styles from './ProductPage.module.css';
-
-
-const ZoomBox = ({zoomStyle}) => {
-  return <div className={styles.zoomBox} style={zoomStyle}></div>;
-};
-
-export default ZoomBox;
